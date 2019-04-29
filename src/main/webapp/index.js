b84f0fb68977c48dbf0ed3e61aef403e8f67be89
Your Name <you@example.com>
2019-04-29 15:29:27 +0200

    Fjernet login

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -7,14 +7,8 @@ function login() {
     var user = $("#loginUser").val();
     var password = $("#loginPass").val();
 
-
-    if (user === "Søren" && password === "1234"){
         show('#admin');
         hide('#login');
-    }
-    else{
-        alert("NEJ")
-    }
 }
 function show(element) {
     $(element).show();
