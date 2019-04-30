284248e3b8b543a07eee7139de64e2d0f941d9b4
theogutt <theodorguttesen@gmail.com>
2019-04-30 12:53:34 +0200

    fjernede overflødig kode

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -54,31 +54,6 @@ function deleteRow(obj) {
 
 }
 
-function addTable() {
-
-    var myTableDiv = document.getElementById("myDynamicTable");
-
-    var table = document.createElement('TABLE');
-    table.border='1';
-
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-
-    for (var i=0; i<3; i++){
-        var tr = document.createElement('TR');
-        tableBody.appendChild(tr);
-
-        for (var j=0; j<4; j++){
-            var td = document.createElement('TD');
-            td.width='75';
-            td.appendChild(document.createTextNode("Cell " + i + "," + j));
-            tr.appendChild(td);
-        }
-    }
-    myTableDiv.appendChild(table);
-
-}
-
 function load() {
 
     console.log("Page load finished");
